dce856f8e8f431b21da45d61654c96cbb6ddcf0d
Antoine Labrecque <antoine.labrecque@yardi.com>
2018-07-13 17:04:51 -0600

    Worked more on number shortening filter.

--- Number_Shortening_Filter/Number_Shortening_Filter.js
+++ Number_Shortening_Filter/Number_Shortening_Filter.js
@@ -1,11 +1,20 @@
 function shortenNumber(suffixes, base) {
   return function filter ( items )
   {
-
-    if ( Number ( items ) )
+    // result is the shortened number.
+    var result = '' ;
+    if ( Number ( items ) == items )
     {
-
+      // suffix is which suffix to use.
+      var suffix = 0 ;
+      for ( ; ( items >= base) && ( suffixes . length > suffix ) ; suffix += 1 )
+      {
+        items /= base ;
+        items -= ( items % base ) ;
+      }
+      items += suffixes [ suffix ] ;
     }
-    return items
+    result += items ;
+    return result ;
   }
 }
