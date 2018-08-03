ffe4b29d20523fc5c2162fc60b636ff009c043fc
Antoine Labrecque <antoine.labrecque@yardi.com>
2018-08-03 17:02:05 -0600

    Worked on dependency injection more.

--- Dependency_Injection/Dependency_Injection.js
+++ Dependency_Injection/Dependency_Injection.js
@@ -9,11 +9,30 @@ var DI = function (dependency) {
 
 // Should return new function with resolved dependencies
 DI.prototype.inject = function (func) {
+  // parametersPointer is an array of the parameters of the function.
+  var parametersPointer = func . toString ( ) . replace ( /.*\(/ , '' ) . split ( ')' , 1 ) ;
+  console.log(parametersPointer);
+  parametersPointer = parametersPointer [ 0 ] . split ( ", " ) ;
+  console.log(parametersPointer);
+  console.log(func.length);
+  do
+  {
+    threwBoolean = false ;
+    try
+    {
+      func ( ) ;
+    }
+    catch ( e )
+    {
+      dep = e . message . split ( " " , 1 ) ;
+      console.log(dep);
+      this . dependency [ dep [ 0 ] ] ;
+      threwBoolean = true ;
+    }
+  }
+  while ( !threwBoolean ) ;
   /* Algorithm 1: resolved ( )
    * The given function with the dependencies resolved.
    * Returns: Whatever the given function would have returned when its dependencies are resolved.*/
-  return function resolved ( )
-  {
-    func ( ) ;
-  } ;
+  return func ("", this.dependency[dep[0]],"" ) ;
 }
