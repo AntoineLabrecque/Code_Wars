1e54339f53fe2d29dba7454a23bd7edcf27c8d10
Antoine Labrecque <antoine.labrecque@yardi.com>
2018-08-17 17:01:11 -0600

    Worked more on dependency injection.

--- Dependency_Injection/Dependency_Injection.js
+++ Dependency_Injection/Dependency_Injection.js
@@ -12,22 +12,32 @@ DI.prototype.inject = function (func) {
   // splitFunctionPointer is the function split up on the character ')'.
   // parametersPointer is an array of the parameters of the function.
   // dependenciesPointer is an array of the dependencies for the function that have been resolved.
-  // bodiesPointer is an array of the bodies of the dependencies for the function that have been resolved.
+  // splitFunctionsPointer is an array of the functions of the dependencies for the function that have been resolved, split up on the character ')'.
+  // functionsPointer is an array of the functions of the dependencies for the function that have been resolved.
+  // functionBodyString is the body of the function with the dependencies resolved.
   var splitFunctionPointer = func . toString ( ) . split ( ')' ) ,
       parametersPointer = splitFunctionPointer [ 0 ] . replace ( /.*\(/ , '' ) . split ( ", " ) ,
       dependenciesPointer = [ ] ,
-      bodiesPointer = [ ] ;
-  splitFunctionPointer . splice ( 0 , 1 ) ;
-  splitFunctionPointer = splitFunctionPointer . join ( ')' ) ;
-  for ( var i = 0 ; i < parametersPointer . length ; i += 1 )
+      splitFunctionsPointer = [ ] ,
+      functionsPointer = [ ] ,
+      functionBodyString = func .toString ( ) . slice ( func . toString ( ) . indexOf ( '{' ) ) ;
+  for ( let i = 0 ; i < parametersPointer . length ; i += 1 )
   {
-    dependenciesPointer . splice ( i , 0 , this . dependency [ parametersPointer [ i ] ] ) ;
-    bodiesPointer . splice ( i , 0 , dependenciesPointer [ i ] . toString ( ) . split ( ')' ) ) ;
-    bodiesPointer [ i ] . splice ( 0 , 1 ) ;
+    if ( this . dependency . hasOwnProperty ( parametersPointer [ i ] ) )
+    {
+      dependenciesPointer . splice ( i , 0 , this . dependency [ parametersPointer [ i ] ] ) ;
+      splitFunctionsPointer . splice ( i , 0 , dependenciesPointer [ i ] . toString ( ) . split ( ')' ) ) ;
+      functionsPointer . splice ( i , 0, new Function ( splitFunctionsPointer [ i ] [ 1 ] . toString ( ) ) ) ;
+      if ( "string" === typeof functionsPointer [ i ] ( ) )
+      {
+        functionBodyString = functionBodyString . replace ( parametersPointer [ i ] + "()" , '\"' + functionsPointer [ i ] ( ) + '\"' ) ;
+      }
+      else
+      {
+        functionBodyString = functionBodyString . replace ( parametersPointer [ i ] + "()" , functionsPointer [ i ] ( ) ) ;
+      }
+    }
   }
-  console.log(dependenciesPointer);
-  var i = "return \"" ;
-  i = i . concat ( func ( this.dependency[parametersPointer[0]] , this.dependency[parametersPointer[1]] , this.dependency[parametersPointer[2]]).toString() + "\" ;") ;
-  console .log (i);
-  return new Function ( i ) ;
+  console.log(typeof this. dependency );
+  return new Function ( functionBodyString ) ;
 }
